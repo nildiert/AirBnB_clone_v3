63047e4c8a09d96b31dd1a5ccec640d56150bcec
nildiert <niljordan23@gmail.com>
2019-09-11 20:33:15 +0000

    [FEATURE]

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Flask app """
+
+from models import storage
+from flask import Flask
+from api.v1.views import app_views
+from os import environ
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_request(exception=None):
+    storage.close()
+
+
+
+
+if __name__ == "__main__":
+    my_host = environ.get("HBNB_API_HOST")
+    my_port = environ.get("HBNB_API_PORT")
+    app.run(host=my_host, port=my_port, threaded=True)
