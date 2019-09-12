aa4b52e9c3b7cee919314fbbb2fe9fd426f3aad7
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-09-12 17:06:17 +0000

    Fixing Create Review Method

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -12,19 +12,19 @@ from api.v1.views import app_views
 @app_views.route('places/<place_id>/reviews', methods=['POST'])
 def create_review(place_id):
     """
-    Create and returns a new a new review with the status code 201
+    Create and returns a new review with the status code 201
     """
     if not request.json:
         abort(400, "Not a JSON")
     if 'user_id' not in request.json:
         abort(400, "Missing user_id")
-    if 'name' not in request.json:
-        abort(400, "Missing name")
+    if 'text' not in request.json:
+        abort(400, "Missing text")
     place = storage.get("Place", place_id)
     user = storage.get("User", id=request.json['user_id'])
     if place and user:
         request.json['place_id'] = place_id
-        review = models.review.Place(**request.json)
+        review = models.review.Review(**request.json)
         review.save()
         return jsonify(review.to_dict()), 201
     abort(404)
