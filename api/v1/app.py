55b1be7de3cb308a7938ad4894be92341decbc07
nildiert <niljordan23@gmail.com>
2019-09-12 00:30:21 +0000

    [FEATURE] Added error handler to manage the 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,15 +12,24 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_request(exception=None):
+    """
+    Method to close the connection when the execution finish
+    """
     storage.close()
 
 @app.errorhandler(404)
 def not_found(error):
+    """
+    Method to manage the url's that doesn't exists
+    """
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
 
 if __name__ == "__main__":
+    """
+    Main function
+    """
     my_host = environ.get("HBNB_API_HOST")
     my_port = environ.get("HBNB_API_PORT")
     app.run(host=my_host, port=my_port, threaded=True)
