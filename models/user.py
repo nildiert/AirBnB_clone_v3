e641ef2eaeb8adeb50c33d4ce2660677e591dee7
nildiert <niljordan23@gmail.com>
2019-09-13 03:43:00 +0000

    [FEATURE] Added md5 file

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,5 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        pass_encode = self.password.encode()
+        self.password = hashlib.md5(pass_encode).hexdigest()
